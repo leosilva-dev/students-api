320e69f0e695994b1879d25823f4f5e005866c57
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-04-01 21:06:05 -0300

    feat: loading env variables throug dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import fastify from 'fastify';
+import database from '../infra/database';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = fastify();
 
-app.get('/', (request, reply) => {
-  return reply.status(200).send({ message: 'hello world' });
+app.get('/', async (request, reply) => {
+  const result = await database.query('select 1+1');
+  return reply.status(200).send({ message: result });
 });
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('Server is running!');
+  console.log('Server is running on port 3333 🔥');
 });
