ed0a0726219b2c9e56f7e077bfb768f4621b6596
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-04-23 16:11:00 -0300

    feat: add routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,29 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-
-type RouteHandler = (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
-
-interface RouteOptions {
-  handler: RouteHandler;
-}
+import { alunosController } from './controllers/alunos';
 
 export default async function routes(fastify: FastifyInstance): Promise<void> {
-  fastify.get('/', async (request, reply) => {
-    reply.send({ message: 'Olá, mundo!' });
+  fastify.get('/alunos', async (request: FastifyRequest, reply: FastifyReply) => {
+    const result = await alunosController.getAll(request, reply);
+    reply.send({ result });
+  });
+
+  fastify.get('/alunos/:id', async (request: FastifyRequest, reply: FastifyReply) => {
+    const result = await alunosController.getById(request, reply);
+    reply.send({ result });
+  });
+
+  fastify.post('/alunos', async (request: FastifyRequest, reply: FastifyReply) => {
+    const result = await alunosController.create(request, reply);
+    reply.send({ result });
+  });
+
+  fastify.delete('/alunos/:id', async (request: FastifyRequest, reply: FastifyReply) => {
+    const result = await alunosController.deleteById(request, reply);
+    reply.send({ result });
+  });
+
+  fastify.put('/alunos/:id', async (request: FastifyRequest, reply: FastifyReply) => {
+    const result = await alunosController.updateById(request, reply);
+    reply.send({ result });
   });
 }
