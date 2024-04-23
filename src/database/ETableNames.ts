8c5fe8e808b23124ff967192c8191e03f0952fb5
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-04-23 16:10:10 -0300

    fix: update model Alunos

--- src/database/ETableNames.ts
+++ src/database/ETableNames.ts
@@ -1,3 +1,3 @@
 export enum ETableNames {
-  aluno = 'aluno',
+  aluno = 'alunos',
 }
--- src/database/models/Aluno.ts
+++ src/database/models/Aluno.ts
@@ -1,5 +1,5 @@
 export interface IAluno {
-  id: number;
+  id: string;
   name: string;
   email: string;
   cpf: string;
